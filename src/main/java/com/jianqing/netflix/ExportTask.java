1abbd4032ff5ea1da28981f6e82e940b167dbf69
jianqing_sun <jianqing_sun@apple.com>
2017-11-02 19:09:39 -0700

    test

--- src/main/java/com/jianqing/netflix/ExportTask.java
+++ src/main/java/com/jianqing/netflix/ExportTask.java
@@ -5,14 +5,17 @@ import org.apache.avro.file.DataFileReader;
 import org.apache.avro.generic.GenericData;
 import org.apache.avro.generic.GenericDatumReader;
 import org.apache.hadoop.fs.Path;
-import org.apache.parquet.avro.AvroSchemaConverter;
-import org.apache.parquet.avro.AvroWriteSupport;
 import org.apache.parquet.hadoop.ParquetWriter;
 import org.apache.parquet.hadoop.metadata.CompressionCodecName;
 import org.apache.parquet.schema.MessageType;
+import parquet.avro.AvroWriteSupport;
 
 import java.io.File;
 import java.io.IOException;
+
+
+
+
 
 /**
  * Created by jianqingsun on 10/28/17.
@@ -29,42 +32,10 @@ public class ExportTask implements TaskInterface {
 
     @Override
     public int run() {
-        File avroScheme = new File("src/main/avro/user.avsc");
-        File avroData  = new File("src/main/resources/avro/users.avro");
+        File avroScheme = new File("src/main/avro/netflix/movie.avsc");
+        File avroData  = new File("/tmp/movie");
         try {
-            Schema avroSchema = new Schema.Parser().parse(avroScheme);
-            // generate the corresponding Parquet schema
-            MessageType parquetSchema = new AvroSchemaConverter().convert(avroSchema);
 
-            // create a WriteSupport object to serialize your Avro objects
-            AvroWriteSupport writeSupport = new AvroWriteSupport(parquetSchema, avroSchema);
-
-            // choose compression scheme
-            CompressionCodecName compressionCodecName = CompressionCodecName.UNCOMPRESSED;
-
-            // set Parquet file block size and page size values
-            int blockSize = 256 * 1024 * 1024;
-            int pageSize = 64 * 1024;
-
-            String outputFilename = "src/main/resources/avro/testuser";
-            Path outputPath = new Path(outputFilename);
-
-            // the ParquetWriter object that will consume Avro GenericRecords
-            ParquetWriter parquetWriter = new ParquetWriter(outputPath,
-                    writeSupport, compressionCodecName, blockSize, pageSize);
-
-            GenericDatumReader<GenericData.Record> datum = new GenericDatumReader<GenericData.Record>();
-
-            DataFileReader<GenericData.Record> reader = new DataFileReader<GenericData.Record>(avroData, datum);
-
-            GenericData.Record record = new GenericData.Record(reader.getSchema());
-            while (reader.hasNext()) {
-                GenericData.Record next = reader.next(record);
-                System.out.println(next);
-                parquetWriter.write(next);
-            }
-            parquetWriter.close();
-            reader.close();
 
 
         } catch (IOException e) {
--- src/main/scala/com/jianqing/scala/netflix/ScalaAnalyticsTask.scala
+++ src/main/scala/com/jianqing/scala/netflix/ScalaAnalyticsTask.scala
@@ -1,24 +1,36 @@
 package com.jianqing.scala.netflix
 
 import com.jianqing.netflix.TaskInterface
-import org.apache.avro.Schema
-import org.apache.spark.sql.SparkSession
+import org.apache.parquet.hadoop.metadata.CompressionCodecName
+import org.apache.spark.{SparkConf, SparkContext}
 
 /**
-  * Created by jianqingsun on 10/29/17.
+  * Created by jianqing_sun on 11/2/17.
   */
 class ScalaAnalyticsTask extends TaskInterface{
-  override def init(): Unit = {}
+  override def init(): Unit = ???
 
   override def run(): Int = {
-  0
-  }
+    val sc = new SparkContext(new SparkConf().setAppName("Analytics Task").setMaster("local[*]"))
+    val sqlContext = new org.apache.spark.sql.SQLContext(sc)
+    sqlContext.setConf("spark.sql.parquet.compression.codec", CompressionCodecName.UNCOMPRESSED.name())
+    val df = sqlContext.read.parquet("/tmp/movieexport")
+    df.registerTempTable("movie")
 
-  override def stop(): Int = {
+    sqlContext.sql("select * from movie").collect.foreach(println)
+
     0
   }
 
-  override def clean(): Unit = {
+  override def stop(): Int = ???
 
+  override def clean(): Unit = ???
+}
+
+
+object Mainn{
+  def main(args: Array[String]): Unit = {
+    val task = new ScalaAnalyticsTask()
+    task.run()
   }
 }
